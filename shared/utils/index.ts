44bc3cd88f70e185621e3d2457632f422458addc
unknown <robindimasin@gmail.com>
2022-03-08 12:25:31 +0800

    changed BASE_URL

--- shared/utils/index.ts
+++ shared/utils/index.ts
@@ -1,6 +1,6 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "book-n-go.vercel.app"
+    ? "https://book-n-go.vercel.app"
     : "http://localhost:3000";
 
 export const HOSTNAME =
