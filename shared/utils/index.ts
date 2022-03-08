6c24358afe518d4222409bc5415b30835a95cae7
unknown <robindimasin@gmail.com>
2022-03-08 12:37:50 +0800

    changed hostname

--- shared/utils/index.ts
+++ shared/utils/index.ts
@@ -1,8 +1,10 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://book-n-go.vercel.app"
+    ? "https://book-n-go.herokuapp.com/"
     : "http://localhost:3000";
 
 export const HOSTNAME =
-  process.env.NODE_ENV === "production" ? "book-n-go.vercel.app" : "localhost";
+  process.env.NODE_ENV === "production"
+    ? "book-n-go.herokuapp.com"
+    : "localhost";
 export const PORT = parseInt(process.env.PORT || "3000", 10);
